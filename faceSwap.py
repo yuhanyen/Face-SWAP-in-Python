fd957a6e30ae59ebcda20087136d68009b3517ef
yuhanyen <yuhanyen@gmail.com>
2017-11-09 08:25:48 +0000

    faceswap-v20171109

--- faceSwap.py
+++ faceSwap.py
@@ -12,8 +12,8 @@ def getPoints(filename):
     detector = dlib.get_frontal_face_detector() #Face detector
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     frame = cv2.imread(filename,1)
-    frame_resized = cv2.resize(frame, (600, 800))
-    print("ok")
+    frame_resized = cv2.resize(frame, (300, 400))
+    print(filename + " -> ok")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     clahe_image = clahe.apply(gray)
@@ -25,7 +25,7 @@ def getPoints(filename):
             points.append((int(shape.part(i).x), int(shape.part(i).y))) 
 
     cv2.imshow("image", frame)
-    cv2.imwrite("1234.jpg",frame)
+    cv2.imwrite(filename[:-4] + "+Getpoint.jpg",frame)
     return points
 # Read points from text file
 def readPoints(path) :
@@ -72,12 +72,14 @@ def calculateDelaunayTriangles(rect, points):
     #create subdiv
     subdiv = cv2.Subdiv2D(rect);
     
+    #print('rect = '+str(rect))
+    
     # Insert points into subdiv
     for p in points:
         subdiv.insert(p) 
     
     triangleList = subdiv.getTriangleList();
-    
+     
     delaunayTri = []
     
     pt = []    
@@ -102,9 +104,12 @@ def calculateDelaunayTriangles(rect, points):
                         ind.append(k)                            
             if len(ind) == 3:                                                
                 delaunayTri.append((ind[0], ind[1], ind[2]))
+                #print('('+str(ind[0])+','+str(ind[1])+','+str(ind[2])+')')
         
         pt = []        
             
+    
+    #print ('the length of delaunayTri = '+str(len(delaunayTri)))
     
     return delaunayTri
         
@@ -159,15 +164,15 @@ if __name__ == '__main__' :
 
     # Read images
     #variable
-    filename1 =sys.argv[1] #'Happy_Man.jpg'
-    filename2 =sys.argv[2] #'test.jpg'
-    filename3 =sys.argv[3] #'Happy_Man.jpg'
+    filename1=sys.argv[1] #'Happy_Man.jpg'
+    filename2=sys.argv[2] #'test.jpg'
+    filename3=sys.argv[3] #'Happy_Man.jpg'
 
     if len(sys.argv)==5:
         OutPutImgPATH=sys.argv[4]
-        OutPutImgPATH=OutPutImgPATH+"OutPut_"+filename3
+        OutPutImgPATH=OutPutImgPATH+filename3[:-4]+"+Output.jpg"
     else:
-        OutPutImgPATH="OutPut_"+filename3
+        OutPutImgPATH=filename3[:-4]+"+Output.jpg"
 
     
     img1 = cv2.imread(filename1);
@@ -176,25 +181,33 @@ if __name__ == '__main__' :
     
     # Read array of corresponding points
     points1 = getPoints(filename1)
-    points2 = getPoints(filename2)##readPoints(filename2 + '.txt')    
+    #readPoints(filename1 + '.txt')
+    points2 = getPoints(filename2)
+    #readPoints(filename2 + '.txt')    
     
     # Find convex hull
     hull1 = []
     hull2 = []
 
     hullIndex = cv2.convexHull(np.array(points2), returnPoints = False)
-          
+    
+    #print('the length of points1 = '+str(len(points1)))
+    #print('the length of points2 = '+str(len(points2)))
+    #print('the length of hullIndex = '+str(len(hullIndex)))
+        
     for i in range(0, len(hullIndex)):
         #print(points1[int(hullIndex[i])])
         hull1.append(points1[int(hullIndex[i])])
         hull2.append(points2[int(hullIndex[i])])
-    
     
     # Find delanauy traingulation for convex hull points
     sizeImg2 = img2.shape    
     rect = (0, 0, sizeImg2[1], sizeImg2[0])
      
     dt = calculateDelaunayTriangles(rect, hull2)
+    
+    #for i in range(0, len(dt)):
+    #    print(str(dt[i]))
     
     if len(dt) == 0:
         quit()
@@ -209,9 +222,18 @@ if __name__ == '__main__' :
             t1.append(hull1[dt[i][j]])
             t2.append(hull2[dt[i][j]])
         
+        #print('t1 ==> ')
+        #for i in range(0, len(t1)):
+        #    print(str(t1[i]))
+            
+        #print('t2 ==> ')
+        #for i in range(0, len(t2)):
+        #    print(str(t2[i]))       
+        
         warpTriangle(img1, img1Warped, t1, t2)
     
-            
+    cv2.imwrite(filename1[:-4]+"+img1Warped.jpg",img1Warped)
+    
     # Calculate Mask
     hull8U = []
     for i in range(0, len(hull2)):
@@ -224,13 +246,15 @@ if __name__ == '__main__' :
     r = cv2.boundingRect(np.float32([hull2]))    
     
     center = ((r[0]+int(r[2]/2), r[1]+int(r[3]/2)))
+    
+    #print('center = '+str(center))
         
     
-    # Clone seamlessly.
+    # Clone seamlessly. Seamless cloning
     output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
     
     #cv2.imshow("Face Swapped", output)
     cv2.imwrite(OutPutImgPATH,output)
     #cv2.waitKey(0)
-    print(OutPutImgPATH)
+    #print('path = '+OutPutImgPATH)
     cv2.destroyAllWindows()
